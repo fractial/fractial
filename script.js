ca95c1c54f926fe459fb4ca79b61b5f7025fe941
Fractial <126729237+fractial@users.noreply.github.com>
2023-12-16 13:51:05 +0100

    Created pages

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.repo-container')
+
+const user = 'fractial';
+
+fetch(`https://api.github.com/users/${user}/repos`)
+    .then(response => response.json())
+    .then(repos => {
+        repos.forEach(repo => {
+            const a = document.createElement('a');
+            a.href = `https://github.com/${user}/${repo.name}`;
+
+            const img = document.createElement('img');
+            img.className = 'repo';
+            img.src = `https://opengraph.githubassets.com/0/${user}/${repo.name}`;
+            img.alt = repo.name;
+
+            a.append(img);
+            container.appendChild(a);
+        });
+    })
+    .catch(error => console.error('Error:', error));
