e9a5d63dc91e2dc830b7421512c596af9f40ad70
Fractial <126729237+fractial@users.noreply.github.com>
2023-12-20 18:28:31 +0000

    Updates UI/UX

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-const container = document.getElementById('repos');
+const div = document.getElementById('repos');
 
-const uuid = length => Array.from({ length }, () => Math.random().toString(36)[2]).join('');
+const name = 'fractial';
 
-const user = 'fractial';
+fetch(`https:api.github.com/users/${name}/repos`)
+.then(response => response.json())
+.then(repos => {
+    repos.forEach(repo => {
+        const a = document.createElement('a');
+        a.className = 'repo';
+        a.href = `https://github.com/${name}/${repo.name}`;
 
-fetch(`https://api.github.com/users/${user}/repos`)
-    .then(response => response.json())
-    .then(repos => {
-        repos.forEach(repo => {
-            const a = document.createElement('a');
-            a.href = `https://github.com/${user}/${repo.name}`;
+        const img = document.createElement('img');
+        img.setAttribute('box-shadow', '')
+        img.src = `https://opengraph.githubassets.com/${Date.now()}/${name}/${repo.name}`;
+        img.alt = repo.name
 
-            const img = document.createElement('img');
-            img.className = 'repo';
-            img.src = `https://opengraph.githubassets.com/${uuid(10)}/${user}/${repo.name}`;
-            img.alt = repo.name;
+        a.append(img)
+        div.appendChild(a);
 
-            a.append(img);
-            container.appendChild(a);
-
-            VanillaTilt.init(a, {
-                max: 2.5,
-                perspective: 1000,
-            });
+        VanillaTilt.init(a, {
+            max: 16,
+            perspective: 1000,
         });
-    })
-    .catch(error => console.error('Error:', error));
+    });
+})
+.catch(error => console.error(error));
